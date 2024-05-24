318aa99312c1a12254497c722a820652e49bd02d
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-05-24 10:32:11 +0200

    ✨Implement XKCD in home view

--- HyruleCompendium/src/app/home/home.component.ts
+++ HyruleCompendium/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { XkcdComponent } from "../xkcd/xkcd.component";
 
 @Component({
   selector: "hyrule-home",
   templateUrl: "./home.component.html",
   standalone: true,
+  imports: [XkcdComponent]
 })
 export class HomeComponent {
 
